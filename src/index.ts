ed22aad9205311efcea78e615381ab74c38d2fcc
Claas Ahlrichs <claasahl@gmail.com>
2018-12-28 10:49:00 +0100

    skip pretty-quick

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,7 @@
 #!/usr/bin/env node
 import execa from "execa";
-import jsonfile from "jsonfile";
 
-const PACKAGE_JSON = "package.json";
 (async () => {
   await execa("npm", ["install", "typescript", "@types/node", "--save-dev"]);
   await execa("tsc", ["--init"]);
-
-  await execa("npm", [
-    "install",
-    "prettier",
-    "pretty-quick",
-    "husky",
-    "--save-dev"
-  ]);
-  const husky = {
-    husky: {
-      hooks: {
-        "pre-commit": "pretty-quick --staged"
-      }
-    }
-  };
-  const pkg = jsonfile.readFileSync(PACKAGE_JSON);
-  jsonfile.writeFileSync(PACKAGE_JSON, { ...husky, ...pkg });
 })();
