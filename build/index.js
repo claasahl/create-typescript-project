066b502fbe997935e6b7281bd21ff079ee35823c
Claas Ahlrichs <claasahl@gmail.com>
2019-01-05 12:00:36 +0100

    does package.json already exist?

--- build/index.js
+++ build/index.js
@@ -7,11 +7,17 @@ var __importDefault =
   };
 Object.defineProperty(exports, "__esModule", { value: true });
 var path_1 = __importDefault(require("path"));
+var fs_1 = __importDefault(require("fs"));
 var dir = process.cwd();
 var packageFile = path_1.default.resolve(dir, "package.json");
-if (packageFile) {
-  console.log("Found package.json:", packageFile);
+if (packageFile && fs_1.default.existsSync(packageFile)) {
+  console.log("Resolved 'package.json':", packageFile);
 } else {
-  console.log("Did not find package.json");
+  console.log("Did not resolve 'package.json'.");
+}
+if (fs_1.default.existsSync(packageFile)) {
+  console.log("'package.json' exists.");
+} else {
+  console.log("'package.json' does not exist.");
 }
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 import path from "path";
+import fs from "fs";
 
 const dir = process.cwd();
 const packageFile = path.resolve(dir, "package.json");
-if (packageFile) {
-  console.log("Found package.json:", packageFile);
+if (packageFile && fs.existsSync(packageFile)) {
+  console.log("Resolved 'package.json':", packageFile);
 } else {
-  console.log("Did not find package.json");
+  console.log("Did not resolve 'package.json'.");
+}
+
+if (fs.existsSync(packageFile)) {
+  console.log("'package.json' exists.");
+} else {
+  console.log("'package.json' does not exist.");
 }
