8801e56a8612f10fed43e6d815ea55af7e355ea1
Claas Ahlrichs <claasahl@gmail.com>
2019-02-10 12:18:29 +0100

    restored installation of TS dep.s

--- build/index.js
+++ build/index.js
@@ -1,23 +1,185 @@
 #!/usr/bin/env node
 "use strict";
+var __awaiter =
+  (this && this.__awaiter) ||
+  function(thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function(resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : new P(function(resolve) {
+              resolve(result.value);
+            }).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+  };
+var __generator =
+  (this && this.__generator) ||
+  function(thisArg, body) {
+    var _ = {
+        label: 0,
+        sent: function() {
+          if (t[0] & 1) throw t[1];
+          return t[1];
+        },
+        trys: [],
+        ops: []
+      },
+      f,
+      y,
+      t,
+      g;
+    return (
+      (g = { next: verb(0), throw: verb(1), return: verb(2) }),
+      typeof Symbol === "function" &&
+        (g[Symbol.iterator] = function() {
+          return this;
+        }),
+      g
+    );
+    function verb(n) {
+      return function(v) {
+        return step([n, v]);
+      };
+    }
+    function step(op) {
+      if (f) throw new TypeError("Generator is already executing.");
+      while (_)
+        try {
+          if (
+            ((f = 1),
+            y &&
+              (t =
+                op[0] & 2
+                  ? y["return"]
+                  : op[0]
+                  ? y["throw"] || ((t = y["return"]) && t.call(y), 0)
+                  : y.next) &&
+              !(t = t.call(y, op[1])).done)
+          )
+            return t;
+          if (((y = 0), t)) op = [op[0] & 2, t.value];
+          switch (op[0]) {
+            case 0:
+            case 1:
+              t = op;
+              break;
+            case 4:
+              _.label++;
+              return { value: op[1], done: false };
+            case 5:
+              _.label++;
+              y = op[1];
+              op = [0];
+              continue;
+            case 7:
+              op = _.ops.pop();
+              _.trys.pop();
+              continue;
+            default:
+              if (
+                !((t = _.trys), (t = t.length > 0 && t[t.length - 1])) &&
+                (op[0] === 6 || op[0] === 2)
+              ) {
+                _ = 0;
+                continue;
+              }
+              if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) {
+                _.label = op[1];
+                break;
+              }
+              if (op[0] === 6 && _.label < t[1]) {
+                _.label = t[1];
+                t = op;
+                break;
+              }
+              if (t && _.label < t[2]) {
+                _.label = t[2];
+                _.ops.push(op);
+                break;
+              }
+              if (t[2]) _.ops.pop();
+              _.trys.pop();
+              continue;
+          }
+          op = body.call(thisArg, _);
+        } catch (e) {
+          op = [6, e];
+          y = 0;
+        } finally {
+          f = t = 0;
+        }
+      if (op[0] & 5) throw op[1];
+      return { value: op[0] ? op[1] : void 0, done: true };
+    }
+  };
 var __importDefault =
   (this && this.__importDefault) ||
   function(mod) {
     return mod && mod.__esModule ? mod : { default: mod };
   };
 Object.defineProperty(exports, "__esModule", { value: true });
+var execa_1 = __importDefault(require("execa"));
+var init_package_json_1 = __importDefault(require("init-package-json"));
 var path_1 = __importDefault(require("path"));
-var fs_1 = __importDefault(require("fs"));
+var HOME = process.env.HOME || ".";
+// a path to a promzard module.  In the event that this file is
+// not found, one will be provided for you.
+var initFile = path_1.default.resolve(HOME, ".npm-init");
+// the dir where we're doin stuff.
 var dir = process.cwd();
-var packageFile = path_1.default.resolve(dir, "package.json");
-if (packageFile && fs_1.default.existsSync(packageFile)) {
-  console.log("Resolved 'package.json':", packageFile);
-} else {
-  console.log("Did not resolve 'package.json'.");
-}
-if (fs_1.default.existsSync(packageFile)) {
-  console.log("'package.json' exists.");
-} else {
-  console.log("'package.json' does not exist.");
-}
+// extra stuff that gets put into the PromZard module's context.
+// In npm, this is the resolved config object.  Exposed as 'config'
+// Optional.
+var configData = { yes: false };
+// Any existing stuff from the package.json file is also exposed in the
+// PromZard module as the `package` object.  There will also be free
+// vars for:
+// * `filename` path to the package.json file
+// * `basename` the tip of the package dir
+// * `dirname` the parent of the package dir
+init_package_json_1.default(dir, initFile, configData, function(_er, _data) {
+  var _this = this;
+  // the data's already been written to {dir}/package.json
+  // now you can do stuff with it
+  (function() {
+    return __awaiter(_this, void 0, void 0, function() {
+      return __generator(this, function(_a) {
+        switch (_a.label) {
+          case 0:
+            return [
+              4 /*yield*/,
+              execa_1.default("npm", [
+                "install",
+                "typescript",
+                "@types/node",
+                "--save-dev"
+              ])
+            ];
+          case 1:
+            _a.sent();
+            return [4 /*yield*/, execa_1.default("tsc", ["--init"])];
+          case 2:
+            _a.sent();
+            return [2 /*return*/];
+        }
+      });
+    });
+  })();
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
 #!/usr/bin/env node
+import execa from "execa";
+import init from "init-package-json";
 import path from "path";
-import fs from "fs";
 
+const HOME = process.env.HOME || ".";
+// a path to a promzard module.  In the event that this file is
+// not found, one will be provided for you.
+const initFile = path.resolve(HOME, ".npm-init");
+
+// the dir where we're doin stuff.
 const dir = process.cwd();
-const packageFile = path.resolve(dir, "package.json");
-if (packageFile && fs.existsSync(packageFile)) {
-  console.log("Resolved 'package.json':", packageFile);
-} else {
-  console.log("Did not resolve 'package.json'.");
-}
 
-if (fs.existsSync(packageFile)) {
-  console.log("'package.json' exists.");
-} else {
-  console.log("'package.json' does not exist.");
-}
+// extra stuff that gets put into the PromZard module's context.
+// In npm, this is the resolved config object.  Exposed as 'config'
+// Optional.
+const configData = { yes: false };
+
+// Any existing stuff from the package.json file is also exposed in the
+// PromZard module as the `package` object.  There will also be free
+// vars for:
+// * `filename` path to the package.json file
+// * `basename` the tip of the package dir
+// * `dirname` the parent of the package dir
+
+init(dir, initFile, configData, function(_er, _data) {
+  // the data's already been written to {dir}/package.json
+  // now you can do stuff with it
+  (async () => {
+    await execa("npm", ["install", "typescript", "@types/node", "--save-dev"]);
+    await execa("tsc", ["--init"]);
+  })();
+});
