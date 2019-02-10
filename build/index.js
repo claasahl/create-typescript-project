c6a4c84d3cfcceca22554f1c0ab8b9e9580a9b72
Claas Ahlrichs <claasahl@gmail.com>
2019-02-10 12:40:51 +0100

    integrated prettier / pretty-quick

--- build/index.js
+++ build/index.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
 "use strict";
+var __assign =
+  (this && this.__assign) ||
+  function() {
+    __assign =
+      Object.assign ||
+      function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+          s = arguments[i];
+          for (var p in s)
+            if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+        }
+        return t;
+      };
+    return __assign.apply(this, arguments);
+  };
 var __awaiter =
   (this && this.__awaiter) ||
   function(thisArg, _arguments, P, generator) {
@@ -137,6 +152,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var execa_1 = __importDefault(require("execa"));
 var init_package_json_1 = __importDefault(require("init-package-json"));
 var path_1 = __importDefault(require("path"));
+var fs_1 = __importDefault(require("fs"));
 var HOME = process.env.HOME || ".";
 // a path to a promzard module.  In the event that this file is
 // not found, one will be provided for you.
@@ -157,6 +173,13 @@ init_package_json_1.default(dir, initFile, configData, function(_er, _data) {
   var _this = this;
   // the data's already been written to {dir}/package.json
   // now you can do stuff with it
+  var packageFile = path_1.default.resolve(dir, "package.json");
+  var husky = {
+    hooks: {
+      "pre-commit": "pretty-quick --staged"
+    }
+  };
+  fs_1.default.writeFileSync(packageFile, __assign({ husky: husky }, _data));
   (function() {
     return __awaiter(_this, void 0, void 0, function() {
       return __generator(this, function(_a) {
@@ -175,6 +198,18 @@ init_package_json_1.default(dir, initFile, configData, function(_er, _data) {
             _a.sent();
             return [4 /*yield*/, execa_1.default("tsc", ["--init"])];
           case 2:
+            _a.sent();
+            return [
+              4 /*yield*/,
+              execa_1.default("npm", [
+                "install",
+                "prettier",
+                "pretty-quick",
+                "husky",
+                "--save-dev"
+              ])
+            ];
+          case 3:
             _a.sent();
             return [2 /*return*/];
         }
